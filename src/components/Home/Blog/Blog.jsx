3504323c3c9042fb93ded0ee134e9ed7580a1bfa
unknown <hossainmdsabbir194@gmail.com>
2023-07-16 00:07:32 +0600

    blog

---
+++ src/components/Home/Blog/Blog.jsx
@@ -0,0 +1,21 @@
+import SectionTitle from "../../../pages/Shared/SectionTitle/SectionTitle";
+
+
+const Blog = () => {
+    return (
+      <div>
+        
+        <div className="row justify-content-center section-title">
+          <SectionTitle
+            heading="Blog"
+            subHeading="Available Right Now"
+            details=" In the history of modern astronomy, there is probably no one
+                  greater leap forward than the building and launch of the space
+                  telescope known as the Hubble."
+          ></SectionTitle>
+        </div>
+      </div>
+    );
+};
+
+export default Blog;
