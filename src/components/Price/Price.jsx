f88c85a853e6111b8f440f2a5f02f750ec3f10db
unknown <hossainmdsabbir194@gmail.com>
2023-07-16 00:03:47 +0600

    price

---
+++ src/components/Price/Price.jsx
@@ -0,0 +1,12 @@
+import Prices from '../Home/Feature/Prices/Prices';
+import './Price.css'
+
+const Price = () => {
+    return (
+        <div style={{marginTop:"150px"}}>
+            <Prices/>
+        </div>
+    );
+};
+
+export default Price;
