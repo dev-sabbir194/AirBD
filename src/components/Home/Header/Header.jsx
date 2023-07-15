12888d5d2db266c48185c83e8c25878cf44e0a43
unknown <hossainmdsabbir194@gmail.com>
2023-07-16 00:06:07 +0600

    header

---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,19 @@
+import Slider from "../../../pages/Shared/Slider/HomeSlider/Slider";
+import Banner from "../Banner/Banner";
+
+
+const Header = () => {
+  return (
+    <div>
+
+
+      <div style={{marginTop:"120px"}}>
+        <Slider />
+      </div>
+    
+      <Banner />
+    </div>
+  );
+};
+
+export default Header;
