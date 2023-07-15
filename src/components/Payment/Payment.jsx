d6a84f6fb14b36a6ecd738222f779878e9eb0833
unknown <hossainmdsabbir194@gmail.com>
2023-07-16 00:04:22 +0600

    payment

---
+++ src/components/Payment/Payment.jsx
@@ -0,0 +1,18 @@
+import './Payment.css'
+const Payment = () => {
+    return (
+      <div>
+        <section className="contact-section" style={{ marginTop: "115px" }}>
+          <div className="contact-image-container">
+            <img
+              src="https://drive.google.com/uc?id=16K8kpq38HKPw4kXkuqGXRAqYc04MZZTT"
+              alt="Contact Background"
+              className="contact-image"
+            />
+          </div>
+        </section>
+      </div>
+    );
+};
+
+export default Payment;
